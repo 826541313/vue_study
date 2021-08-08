a5a2b9d4d9b3a9e7adf7a029fa5a9a3af9cabef6
18733602599 <82651313@qq.com>
2021-08-08 16:42:34 +0800

    scoped样式

---
+++ vue_test/src copy 6/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生性别: {{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      msg: "你好啊",
+      sex: "男",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.test {
+  background-color: skyblue;
+}
+</style>
