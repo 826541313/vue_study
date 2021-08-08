ea211f9c0326edb499b4a1c7888fa25ef1a39922
18733602599 <82651313@qq.com>
2021-08-08 15:59:40 +0800

    mixin混入（合）

---
+++ vue_test/src copy 4/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School />
+    <Student name="李四" sex="男" :age="18" />
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
---
+++ vue_test/src copy 4/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2 @click="showName">学生姓名: {{ name }}</h2>
+    <h2>学生性别: {{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+// 引入一个mixin
+export default {
+  // el: '#root', // 组件定义时，一定不要写el配置项，因为最终所有的组件都要被个vm管理， 由vm决定服务于哪个容器。
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      msg: "你好啊",
+      sex: "男",
+    };
+  },
+};
+</script>
