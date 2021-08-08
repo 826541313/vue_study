54cee59094fb62beaebf9f20f0ca5f00f0abf2ac
18733602599 <82651313@qq.com>
2021-08-08 16:17:23 +0800

    插件

---
+++ vue_test/src copy 5/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>学校姓名: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等等）
+export default {
+  // el: '#root', // 组件定义时，一定不要写el配置项，因为最终所有的组件都要被个vm管理， 由vm决定服务于哪个容器。
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京昌平",
+    };
+  },
+};
+</script>
+
+<style>
+/* 组件样式 */
+.demo {
+  background-color: orange;
+}
+</style>
---
+++ vue_test/src copy 5/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生性别: {{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      msg: "你好啊",
+      sex: "男",
+    };
+  },
+};
+</script>
