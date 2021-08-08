d26403c48076839b5aba6ea4d5f9262cf0769fc5
18733602599 <82651313@qq.com>
2021-08-08 15:52:21 +0800

    ref属性

---
+++ vue_test/src copy 2/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo" />
+    <School />
+    <Student></Student>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM">点我输出上方的DOM元素</button>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+  data() {
+    return {
+      msg: "欢迎学习Vue！",
+    };
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.title);
+    },
+  },
+};
+</script>
