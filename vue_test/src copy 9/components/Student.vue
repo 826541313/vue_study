ff37d5af97b83c6f2babfba5156d494835141471
18733602599 <82651313@qq.com>
2021-08-09 15:27:47 +0800

    自定义事件

---
+++ vue_test/src copy 9/components/Student.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="test">
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生性别: {{ sex }}</h2>
+    <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑atguigu事件</button>
+    <button @click="death">销毁当前Student组件的实例(vc)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      sex: "男",
+    };
+  },
+  methods: {
+    add() {
+      console.log(" add回调被调用了");
+      this.number++;
+    },
+    sendStudentName() {
+      // 触发Student组件实例身上的atguigu事件
+
+      this.$emit("atguigu", this.name);
+    },
+    unbind() {
+      this.$off("atguigu"); // 解绑一个自定义事件
+      // this.$off(['atguigu,'demo']) // 解绑多个自定义事件
+      // this.$off() // 解绑所有自定义事件
+    },
+    death() {
+      this.$destroy(); //销毁了当前Student组件的实例，销毁后所有Student实例的自定义事件全都不奏效。
+    },
+  },
+};
+</script>
+
+<style scoped>
+.test {
+  background-color: skyblue;
+}
+</style>
