9680b8bc858313255f56570d2aa25cdaddcc30c9
18733602599 <82651313@qq.com>
2021-08-09 15:28:00 +0800

    全局事件总线

---
+++ vue_test/src copy 10/components/Student.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生性别: {{ sex }}</h2>
+    <h2>学生年龄: {{ age }}</h2>
+    <button @click="sendStudentName">把学生名给School组件</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      msg: "你好啊",
+    };
+  },
+  props: ["name", "sex", "age"],
+  methods: {
+    sendStudentName() {
+      this.$bus.$emit("hello", this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.test {
+  background-color: skyblue;
+}
+</style>
---
+++ vue_test/src copy 10/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+Vue.config.productionTip = false
+
+// 应用插件
+
+/*
+  关于不同版本的Vue: 
+    1.vue.js与vue.runtime.xxx.js的区别:
+      (1). vue.js是完整版的Vue,包含:核心功能+模板解析器。
+      (2). vue.runtime.xxx.js是运行版的Vue,只包含:核心功能;没有模板解析器
+
+    2.因为vue.runtime.XXx.js没有模板解析器，所以不能使用template配置项，需要使用
+      render函数接收到的createElement函数去指定具体内容。
+
+*/
+
+new Vue({
+  render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus = this // 全局事件总线
+  }
+}).$mount('#app')
