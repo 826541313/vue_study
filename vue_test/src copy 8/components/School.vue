b7bab2d104f8d1347925187288a9d2f0e09fb50a
18733602599 <82651313@qq.com>
2021-08-09 13:44:42 +0800

    $emit

---
+++ vue_test/src copy 8/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="demo">
+    <h2>学校姓名: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+    <button @click="sendSchoolName">把学校名给App</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  props: ["getSchoolName"],
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京昌平",
+    };
+  },
+  methods: {
+    sendSchoolName() {
+      this.getSchoolName(this.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* 组件样式 */
+.demo {
+  background-color: orange;
+}
+</style>
