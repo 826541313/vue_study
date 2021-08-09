a4f497e19623ce5839f17a717f3fc0074cbbd3dc
18733602599 <82651313@qq.com>
2021-08-09 20:02:49 +0800

    TodoList_动画

---
+++ vue_test/src copy 16/App.vue
@@ -0,0 +1,157 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addtodo="addtodo" />
+        <MyList :todos="todos" />
+        <MyFooter
+          :doneLength="doneLength"
+          :length="length"
+          :checkAllTodo="checkAllTodo"
+          :clearAllTodo="clearAllTodo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import pubsub from "pubsub-js";
+
+import MyHeader from "./components/MyHeader.vue";
+import MyList from "./components/MyList.vue";
+import MyFooter from "./components/MyFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyHeader,
+    MyFooter,
+    MyList,
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(value) {
+        localStorage.setItem("todos", JSON.stringify(value));
+      },
+    },
+  },
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem("todos")) || [],
+    };
+  },
+
+  computed: {
+    doneLength() {
+      return this.todos.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0);
+    },
+    length() {
+      return this.todos.length;
+    },
+    isAll() {
+      return this.doneLength === this.length && this.length > 0;
+    },
+  },
+
+  methods: {
+    // 添加一个todo
+    addtodo(todo) {
+      console.log("我是App组件，我收到了数据：", todo);
+      this.todos.unshift(todo);
+    },
+
+    // 勾选or取消勾选一个todo
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.done = !todo.done;
+      });
+    },
+
+    updateTodo(id, title) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.title = title;
+      });
+    },
+
+    deleteTodo(_, id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done;
+      });
+    },
+
+    clearAllTodo() {
+      this.todos = this.todos.filter((todo) => !todo.done);
+    },
+  },
+
+  mounted() {
+    this.$bus.$on("checkTodo", this.checkTodo);
+    this.$bus.$on("updateTodo", this.updateTodo);
+    this.pubId = pubsub.subscribe("deleteTodo", this.deleteTodo);
+  },
+  beforeDestroy() {
+    this.$bus.$off("checkTodo");
+    this.$bus.$off("updateTodo");
+    pubsub.unsubscribe(this.pubId);
+  },
+};
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-edit {
+  color: #fff;
+  background-color: skyblue;
+  border: 1px solid rgb(87, 134, 153);
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
