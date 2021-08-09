94a48452d4a4920d414a30823ef04d9eb7a2b1bb
18733602599 <82651313@qq.com>
2021-08-09 19:56:08 +0800

    过渡与动画

---
+++ vue_test/src copy 15/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="root">
+    <Test />
+    <Test2 />
+    <Test3 />
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Test from "./components/Test.vue";
+import Test2 from "./components/Test2.vue";
+import Test3 from "./components/Test3.vue";
+
+export default {
+  name: "App",
+  components: {
+    Test,
+    Test2,
+    Test3,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_test/src copy 15/components/Test2.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition-group name="hello" appear>
+      <h1 v-show="isShow" key="1">你好啊！</h1>
+      <h1 v-show="isShow" key="2">尚硅谷!</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+  /* transition: 1s; */
+}
+
+.hello-enter,
+.hello-leave-to {
+  transform: translateX(-100%);
+}
+
+.hello-enter-to.,
+.hello-leave {
+  transform: translateX(0);
+}
+
+.hello-enter-active,
+.hello-leave-active {
+  transition: 1s;
+}
+</style>
