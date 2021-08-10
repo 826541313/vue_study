7cf33e4d4580e63ae9b70e66455912a4a6d03c9e
18733602599 <82651313@qq.com>
2021-08-10 17:24:46 +0800

    mapState与mapGetters

---
+++ vue_test/src copy 21/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Count />
+  </div>
+</template>
+
+<script>
+// 引入组件
+
+import Count from "./components/Count.vue";
+
+export default {
+  name: "App",
+  components: {
+    Count,
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ vue_test/src copy 21/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import vueResource from 'vue-resource'
+
+import store from './store'
+
+
+Vue.config.productionTip = false
+
+// 应用插件
+Vue.use(vueResource)
+
+/*
+  关于不同版本的Vue: 
+    1.vue.js与vue.runtime.xxx.js的区别:
+      (1). vue.js是完整版的Vue,包含:核心功能+模板解析器。
+      (2). vue.runtime.xxx.js是运行版的Vue,只包含:核心功能;没有模板解析器
+
+    2.因为vue.runtime.XXx.js没有模板解析器，所以不能使用template配置项，需要使用
+      render函数接收到的createElement函数去指定具体内容。
+
+*/
+
+new Vue({
+  render: h => h(App),
+  store,
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  }
+}).$mount('#app')
