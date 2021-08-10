14e0c910bbf8b61a75b5a65167731acfb90b8357
18733602599 <82651313@qq.com>
2021-08-10 09:26:53 +0800

    修改

---
+++ vue_test/vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // 开启代理服务器（方式一）
+    // devServer: {
+    //     proxy: 'http://localhost:5000'
+    // }
+
+    // 开启代理服务器（方式二）
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }
+            },
+            '/demo': {
+                target: 'http://localhost:5001',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: { '^/demo': '' }
+            }
+        }
+    }
+}
