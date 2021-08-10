63bf1f3beed1ad9f0e7d9f93bc2af3cd6d9c188f
18733602599 <82651313@qq.com>
2021-08-10 13:12:27 +0800

    作用域插槽

---
+++ vue_test/22_插槽/3_src_作用域插槽/components/Category.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <slot :games="games"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data() {
+    return {
+      games: ["红色警戒", "穿越火线", "劲舞团", "超级玛丽"],
+    };
+  },
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
