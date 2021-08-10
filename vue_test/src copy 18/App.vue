fa57de39671738752b49a2dce47855bba3b1e6ad
18733602599 <82651313@qq.com>
2021-08-10 10:07:13 +0800

    github搜索案例

---
+++ vue_test/src copy 18/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <Search />
+      <List />
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import List from "./components/List.vue";
+// 引入组件
+
+export default {
+  name: "App",
+  components: {
+    Search,
+    List,
+  },
+};
+</script>
