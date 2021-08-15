b2e727382b4a64e8e650b7bec5b810f4af2c5bed
18733602599 <82651313@qq.com>
2021-08-15 17:50:52 +0800

    router-link的replace属性

---
+++ vue_test/src copy 31/pages/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li>{{ id }}</li>
+    <li>{{ title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  props: ['id', 'title']
+};
+</script>
+
+<style>
+</style>
