31014c6cfe665f86f02ae97272fd0f7c62cd4138
18733602599 <826541313@qq.com>
2021-10-14 14:22:37 +0800

    学习

---
+++ antd-demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <a-tag>Tag 1</a-tag>
+    <a-tag><a href="https://github.com/vueComponent/ant-design">Link</a></a-tag>
+    <a-tag closable @close="log">Tag 2</a-tag>
+    <a-tag closable @close.prevent>Prevent Default</a-tag>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Tag from 'ant-design-vue/lib/tag';
+import 'ant-design-vue/lib/tag/style/css';
+export default defineComponent({
+  name: 'App',
+  components: {
+    ATag: Tag
+  },
+  setup() {
+    const log = (e: MouseEvent) => {
+      console.log(e);
+    };
+
+    return {
+      log
+    };
+  }
+});
+</script>
