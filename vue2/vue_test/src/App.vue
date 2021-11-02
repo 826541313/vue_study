4e91316c6b29bb87e79fd2435643087aab09518e
18733602599 <826541313@qq.com>
2021-11-02 16:58:33 +0800

    vue-study

--- vue2/vue_test/src/App.vue
+++ vue2/vue_test/src/App.vue
@@ -1,16 +1,6 @@
 <template>
-  <div>
-    <button>原生的按钮</button>
-    <input type="text" />
-
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+  <div class="app">
+    <a-tag class="text-tag">Tag 1</a-tag>
   </div>
 </template>
 
@@ -18,7 +8,15 @@
 // 引入组件
 
 export default {
-  name: 'App',
-  components: {}
+  name: 'App'
 };
 </script>
+
+<style scoped>
+.app {
+  margin-top: 20px;
+}
+.text-tag {
+  margin-left: 20px;
+}
+</style>
